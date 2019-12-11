6f32b88c64fba5fea94dd22a9ce3124db6083872
yuzhuang.jiang <jiangyuzhuang1989@163.com>
2019-12-11 14:41:18 +0800

    新增多层页面

--- zhjdsd/js/common.js
+++ zhjdsd/js/common.js
@@ -1,5 +1,8 @@
-var ALLCLASS = [{
+var ALLCLASS = [
+{
     name: '一年级',
+    title:'古诗词',
+    electiveTitle:'经典选篇',
     poetryList: [{
             title: '大风歌',
             id: 1
@@ -97,6 +100,18 @@ var ALLCLASS = [{
             id: 24
         },
     ],
+    electiveList:[
+        {
+            title: '三字经',
+            id: 1,
+            href:'szj.html'
+        },
+        {
+            title: '百家姓(节选)',
+            id: 2,
+            href:'bjx.html'
+        }
+    ],
     id: 1
 }, {
     name: '二年级',
@@ -108,7 +123,222 @@ var ALLCLASS = [{
     id: 3
 }, {
     name: '四年级',
-    poetryList: [],
+    title:'古诗词',
+    electiveTitle:'经典选篇·孝经',
+    poetryList: [{
+            title: '人日思归',
+            id: 1
+        },
+        {
+            title: '蝉',
+            id: 2
+        },
+        {
+            title: '在狱咏蝉',
+            id: 3
+        },
+        {
+            title: '凉州词',
+            id: 4
+        },
+        {
+            title: '出  塞',
+            id: 5
+        },
+        {
+            title: '终南望余雪',
+            id: 6
+        },
+        {
+            title: '鹿  柴',
+            id: 7
+        },
+        {
+            title: '别董大',
+            id: 8
+        },
+        {
+            title: '江畔独步寻花(其五)',
+            id: 9
+        },
+        {
+            title: '八阵图',
+            id: 10
+        },
+        {
+            title: '城东早春',
+            id: 11
+        },
+        {
+            title: '早春呈水部张十八员外',
+            id: 12
+        },
+        {
+            title: '浪淘沙',
+            id: 13
+        },
+        {
+            title: '竹枝词',
+            id: 14
+        },
+        {
+            title: '暮江吟',
+            id: 15
+        },
+        {
+            title: '菊  花',
+            id: 16
+        },
+        {
+            title: '金缕衣',
+            id: 17
+        },
+        {
+            title: '南  园(其五)',
+            id: 18
+        },
+        {
+            title: '寄扬州韩绰判官',
+            id: 19
+        },
+        {
+            title: '过华清宫',
+            id: 20
+        },
+        {
+            title: '嫦  娥',
+            id: 21
+        },
+        {
+            title: '台  城',
+            id: 22
+        },
+        {
+            title: '泊船瓜洲',
+            id: 23
+        },
+        {
+            title: '题西林壁',
+            id: 24
+        },{
+            title: '春  宵',
+            id: 25
+        },{
+            title: '夏日绝句',
+            id: 26
+        },{
+            title: '如梦令',
+            id: 27
+        },{
+            title: '冬夜读书示子聿',
+            id: 28
+        },{
+            title: '秋夜将晓出篱门迎凉有感(其二)',
+            id: 29
+        },{
+            title: '宿新市徐公店',
+            id: 30
+        },{
+            title: '雪  梅',
+            id: 31
+        },{
+            title: '绝  句',
+            id: 32
+        },{
+            title: '乡村四月',
+            id: 33
+        },{
+            title: '墨  梅',
+            id: 34
+        },
+    ],
+    electiveList:[
+        {
+            title: '开宗明义章第一',
+            id: 1,
+            href:''
+        },
+        {
+            title: '纪孝行章第十',
+            id: 2,
+            href:''
+        },{
+            title: '天子章第二',
+            id: 3,
+            href:''
+        },
+        {
+            title: '五刑章第十一',
+            id: 4,
+            href:''
+        },
+        {
+            title: '诸侯章第三',
+            id: 5,
+            href:''
+        },{
+            title: '广要道章第十二',
+            id: 6,
+            href:''
+        },
+        {
+            title: '卿、大夫章第四',
+            id: 7,
+            href:''
+        },{
+            title: '广至德章第十三',
+            id: 8,
+            href:''
+        },
+        {
+            title: '士章第五',
+            id: 9,
+            href:''
+        },
+        
+        {
+            title: '广扬名章第十四',
+            id: 10,
+            href:''
+        },
+        {
+            title: '庶人章第六',
+            id: 11,
+            href:''
+        },
+        {
+            title: '谏诤章第十五',
+            id: 12,
+            href:''
+        },{
+            title: '三才章第七',
+            id: 13,
+            href:''
+        },
+        {
+            title: '感应章第十六',
+            id: 14,
+            href:''
+        },
+        {
+            title: '孝治章第八',
+            id: 15,
+            href:''
+        },{
+            title: '事君章第十七',
+            id: 16,
+            href:''
+        },
+        {
+            title: '圣治章第九',
+            id: 17,
+            href:''
+        },{
+            title: '丧亲章第十八',
+            id: 18,
+            href:''
+        }
+    ],
     id: 4
 }, {
     name: '五年级',
@@ -150,11 +380,18 @@ function getAllClass() {
     }
     return arr
 }
-
-function getPoetryList(id) {
+function getSelectObj(id){
+    for (var i = 0; i < ALLCLASS.length; i++) {
+        if(id==ALLCLASS[i].id){
+            return ALLCLASS[i]
+        }
+    }
+    return null
+}
+function getPoetryList(id,listType) {
     for (var i = 0; i < ALLCLASS.length; i++) {
         if (ALLCLASS[i].id == id) {
-            return ALLCLASS[i].poetryList;
+            return ALLCLASS[i][listType];
         }
     }
 }
@@ -221,4 +458,16 @@ function pre(){
     var id = getUrlParam('id');
     var location = window.location.href.substr(0,window.location.href.indexOf('?'));
     window.location.href = location+'?id='+(Number(id)-1)
+}
+function nextPage(){
+    var page = getUrlParam('page') ? getUrlParam('page') : 1;
+    var id =  getUrlParam('id');
+    var location = window.location.href.substr(0,window.location.href.indexOf('?'));
+    window.location.href = location+'?id='+id+'&page='+(Number(page)+1)
+}
+function prePage(){
+    var id =  getUrlParam('id');
+    var page = getUrlParam('page') ? getUrlParam('page') : 1;
+    var location = window.location.href.substr(0,window.location.href.indexOf('?'));
+    window.location.href = location+'?id='+id+'&page='+(Number(page)-1)
 }
