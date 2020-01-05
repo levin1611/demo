22e8b6ce34a2064e33fe937ca91073b1b629bc18
姜子牙的姜 <869598419@qq.com>
2020-01-05 21:49:28 +0800

    zhyw

---
+++ zhyw/js/common.js
@@ -0,0 +1,566 @@
+var ALLCLASS = [
+    {
+        name: '一年级',
+        title: '古诗词',
+        electiveTitle: '经典选篇',
+        poetryList: [{
+            title: '大风歌',
+            id: 1,
+            subtitle: '',
+            dynasty: '汉',
+            author: '刘邦',
+        },
+        {
+            title: '静夜思',
+            id: 9,
+            subtitle: '',
+            dynasty: '唐',
+            author: '李白',
+        },
+        {
+            title: '池  上',
+            subtitle: '',
+            dynasty: '唐',
+            author: '白居易',
+            id: 18
+        },
+        {
+            title: '江  南',
+            id: 2,
+            subtitle: '',
+            dynasty: '',
+            author: '汉乐府',
+        },
+        {
+            title: '赠汪伦',
+            id: 10,
+            subtitle: '',
+            dynasty: '唐',
+            author: '李白',
+        },
+        {
+            title: '寻隐者不遇',
+            subtitle: '',
+            dynasty: '唐',
+            author: '贾岛',
+            id: 19
+        },
+        {
+            title: '咏  鹅',
+            id: 3,
+            subtitle: '',
+            dynasty: '唐',
+            author: '骆宾王',
+        },
+        {
+            title: '古朗月行',
+            id: 11,
+            subtitle: '节选',
+            dynasty: '唐',
+            author: '李白',
+        },
+        {
+            title: '金缕衣',
+            subtitle: '',
+            dynasty: '唐',
+            author: '杜秋娘',
+            id: 15
+        },
+        {
+            title: '风',
+            id: 4,
+            subtitle: '',
+            dynasty: '唐',
+            author: '李峤',
+        },
+        {
+            title: '秋浦歌',
+            subtitle: '',
+            dynasty: '唐',
+            author: '李白',
+            id: 12
+        },
+        {
+            title: '乐游原',
+            subtitle: '',
+            dynasty: '唐',
+            author: '李商隐',
+            id: 20
+        },
+        {
+            title: '渡汉江',
+            id: 5,
+            subtitle: '',
+            dynasty: '唐',
+            author: '宋之问',
+        },
+        {
+            title: '听弹琴',
+            subtitle: '',
+            dynasty: '唐',
+            author: '刘长卿',
+            id: 13
+        },
+        {
+            title: '山村咏怀',
+            subtitle: '',
+            dynasty: '宋',
+            author: '邵雍',
+            id: 21
+        },
+        {
+            title: '送  兄',
+            id: 6,
+            subtitle: '',
+            dynasty: '唐',
+            author: '七岁女童',
+        },
+        {
+            title: '前出塞',
+            subtitle: '节选',
+            dynasty: '唐',
+            author: '杜甫',
+            id: 14
+        },
+        {
+            title: '小  池',
+            subtitle: '',
+            dynasty: '宋',
+            author: '杨万里',
+            id: 22
+        },
+        {
+            title: '春  晓',
+            id: 7,
+            subtitle: '',
+            dynasty: '唐',
+            author: '孟浩然',
+        },
+        {
+            title: '悯  农',
+            subtitle: '其二',
+            dynasty: '唐',
+            author: '李绅',
+            id: 16
+        },
+        {
+            title: '画  鸡',
+            subtitle: '',
+            dynasty: '唐',
+            author: '唐寅',
+            id: 23
+        },
+        {
+            title: '山  中',
+            id: 8,
+            subtitle: '',
+            dynasty: '唐',
+            author: '王维',
+        },
+        {
+            title: '问刘十九',
+            subtitle: '',
+            dynasty: '唐',
+            author: '白居易',
+            id: 17
+        },
+        {
+            title: '画',
+            subtitle: '',
+            dynasty: '',
+            author: '',
+            id: 24
+        },
+        {
+            title: '渔家傲.秋思',
+            id: 25,
+            subtitle: '',
+            dynasty: '宋',
+            author: '范仲淹',
+        },
+        {
+            title: '问刘十九',
+            subtitle: '',
+            dynasty: '唐',
+            author: '白居易',
+            id: 17
+        },
+        {
+            title: '画',
+            subtitle: '',
+            dynasty: '',
+            author: '',
+            id: 24
+        },
+        {
+            title: '山  中',
+            id: 8,
+            subtitle: '',
+            dynasty: '唐',
+            author: '王维',
+        },
+        {
+            title: '问刘十九',
+            subtitle: '',
+            dynasty: '唐',
+            author: '白居易',
+            id: 17
+        },
+        {
+            title: '画',
+            subtitle: '',
+            dynasty: '',
+            author: '',
+            id: 24
+        },
+        {
+            title: '山  中',
+            id: 8,
+            subtitle: '',
+            dynasty: '唐',
+            author: '王维',
+        },
+        {
+            title: '问刘十九',
+            subtitle: '',
+            dynasty: '唐',
+            author: '白居易',
+            id: 17
+        },
+        {
+            title: '画',
+            subtitle: '',
+            dynasty: '',
+            author: '',
+            id: 24
+        },
+        {
+            title: '山  中',
+            id: 8,
+            subtitle: '',
+            dynasty: '唐',
+            author: '王维',
+        },
+        {
+            title: '问刘十九',
+            subtitle: '',
+            dynasty: '唐',
+            author: '白居易',
+            id: 17
+        },
+        {
+            title: '画',
+            subtitle: '',
+            dynasty: '',
+            author: '',
+            id: 24
+        }
+        ],
+        id: 1
+    }, {
+        name: '二年级',
+        poetryList: [],
+        id: 2
+    }, {
+        name: '三年级',
+        poetryList: [],
+        id: 3
+    }, {
+        name: '四年级',
+        title: '古诗词',
+        electiveTitle: '经典选篇·孝经',
+        poetryList: [{
+            title: '人日思归',
+            id: 1
+        },
+        {
+            title: '蝉',
+            id: 2
+        },
+        {
+            title: '在狱咏蝉',
+            id: 3
+        },
+        {
+            title: '凉州词',
+            id: 4
+        },
+        {
+            title: '出  塞',
+            id: 5
+        },
+        {
+            title: '终南望余雪',
+            id: 6
+        },
+        {
+            title: '鹿  柴',
+            id: 7
+        },
+        {
+            title: '别董大',
+            id: 8
+        },
+        {
+            title: '江畔独步寻花(其五)',
+            id: 9
+        },
+        {
+            title: '八阵图',
+            id: 10
+        },
+        {
+            title: '城东早春',
+            id: 11
+        },
+        {
+            title: '早春呈水部张十八员外',
+            id: 12
+        },
+        {
+            title: '浪淘沙',
+            id: 13
+        },
+        {
+            title: '竹枝词',
+            id: 14
+        },
+        {
+            title: '暮江吟',
+            id: 15
+        },
+        {
+            title: '菊  花',
+            id: 16
+        },
+        {
+            title: '金缕衣',
+            id: 17
+        },
+        {
+            title: '南  园(其五)',
+            id: 18
+        },
+        {
+            title: '寄扬州韩绰判官',
+            id: 19
+        },
+        {
+            title: '过华清宫',
+            id: 20
+        },
+        {
+            title: '嫦  娥',
+            id: 21
+        },
+        {
+            title: '台  城',
+            id: 22
+        },
+        {
+            title: '泊船瓜洲',
+            id: 23
+        },
+        {
+            title: '题西林壁',
+            id: 24
+        }, {
+            title: '春  宵',
+            id: 25
+        }, {
+            title: '夏日绝句',
+            id: 26
+        }, {
+            title: '如梦令',
+            id: 27
+        }, {
+            title: '冬夜读书示子聿',
+            id: 28
+        }, {
+            title: '秋夜将晓出篱门迎凉有感(其二)',
+            id: 29
+        }, {
+            title: '宿新市徐公店',
+            id: 30
+        }, {
+            title: '雪  梅',
+            id: 31
+        }, {
+            title: '绝  句',
+            id: 32
+        }, {
+            title: '乡村四月',
+            id: 33
+        }, {
+            title: '墨  梅',
+            id: 34
+        },
+        ],
+        electiveList: [
+            {
+                title: '开宗明义章第一',
+                id: 1,
+                href: ''
+            },
+            {
+                title: '纪孝行章第十',
+                id: 2,
+                href: ''
+            }, {
+                title: '天子章第二',
+                id: 3,
+                href: ''
+            },
+            {
+                title: '五刑章第十一',
+                id: 4,
+                href: ''
+            },
+            {
+                title: '诸侯章第三',
+                id: 5,
+                href: ''
+            }, {
+                title: '广要道章第十二',
+                id: 6,
+                href: ''
+            },
+            {
+                title: '卿、大夫章第四',
+                id: 7,
+                href: ''
+            }, {
+                title: '广至德章第十三',
+                id: 8,
+                href: ''
+            },
+            {
+                title: '士章第五',
+                id: 9,
+                href: ''
+            },
+
+            {
+                title: '广扬名章第十四',
+                id: 10,
+                href: ''
+            },
+            {
+                title: '庶人章第六',
+                id: 11,
+                href: ''
+            },
+            {
+                title: '谏诤章第十五',
+                id: 12,
+                href: ''
+            }, {
+                title: '三才章第七',
+                id: 13,
+                href: ''
+            },
+            {
+                title: '感应章第十六',
+                id: 14,
+                href: ''
+            },
+            {
+                title: '孝治章第八',
+                id: 15,
+                href: ''
+            }, {
+                title: '事君章第十七',
+                id: 16,
+                href: ''
+            },
+            {
+                title: '圣治章第九',
+                id: 17,
+                href: ''
+            }, {
+                title: '丧亲章第十八',
+                id: 18,
+                href: ''
+            }
+        ],
+        id: 4
+    }, {
+        name: '五年级',
+        poetryList: [],
+        id: 5
+    }, {
+        name: '六年级',
+        poetryList: [],
+        id: 6
+    }, {
+        name: '七年级',
+        poetryList: [],
+        id: 7
+    }, {
+        name: '八年级',
+        poetryList: [],
+        id: 8
+    }, {
+        name: '九年级',
+        poetryList: [],
+        id: 9
+    }]
+function goBack() {
+    window.history.back();
+}
+
+function getAllClass() {
+    var arr = [];
+    for (var i = 0; i < ALLCLASS.length; i++) {
+        var obj = {
+            name: ALLCLASS[i].name,
+            id: ALLCLASS[i].id
+        }
+        arr.push(obj);
+    }
+    return arr
+}
+
+function getPoetryList(id) {
+    for (var i = 0; i < ALLCLASS.length; i++) {
+        if (ALLCLASS[i].id == id) {
+            return ALLCLASS[i].poetryList;
+        }
+    }
+}
+function getIntention(parentid, id) {
+    var poetryList = getPoetryList(parentid);
+    for (var i = 0; i < poetryList.length; i++) {
+        if (poetryList[i].id == id) {
+            return poetryList[i];
+        }
+
+    }
+}
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var param = window.location.search.substr(1).match(reg);
+    if (param != null) {
+        return unescape(param[2]);
+    }
+    return null;
+}
+
+function audioPlay(id) {
+    $('#audioBox').fadeIn();
+    var audio1 = 'http://music.163.com/song/media/outer/url?id=435289279.mp3';
+    var audio2 = 'https://www.runoob.com/try/demo_source/horse.mp3';
+    bindDrag(document.getElementById('audioBox'));
+    var audio = document.getElementById('audio');
+    if (audio) {
+        if (id == 'audioYin') {
+            $('#audio').attr('src', audio1)
+        } else {
+            $('#audio').attr('src', audio2)
+        }
+
+        $('#' + id).fadeIn();
+        audio.load();
+        audio.play();
+    }
+}
