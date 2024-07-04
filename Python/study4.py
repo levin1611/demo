194428bbf9fde00a83d3eb53609c6fe828fe5bb6
jiangyuzhuang <869598419@qq.com>
2024-07-04 12:16:20 +0800

    1

--- Python/study4.py
+++ Python/study4.py
@@ -39,3 +39,30 @@ for site in sites:
 else:
     print("没有循环数据!")
 print("完成循环!")
+
+
+for n in range(2,10):
+    for x in range(2,n):
+       if n % x == 0:
+          print (n, '等于', x, '*', n//x)
+          break
+    else:
+       print(n, '是质数')
+
+for letter in 'Runoob': 
+   if letter == 'o':
+      pass
+      print ('执行 pass 块')
+   print ('当前字母 :', letter)
+ 
+print ("Good bye!")
+
+a, b = 0, 1
+while b < 10:
+    print(b)
+    a, b = b, a+b
+
+a, b = 0, 1
+while b < 1000:
+    print(b, end=',')
+    a, b = b, a+b
